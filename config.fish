b2f43b0cfb94c7036dfd6fa122e5afdc112c49ee
foldik <foldi.kristof@gmail.com>
2019-07-01 22:27:42 +0200

    Add make path

--- config.fish
+++ config.fish
@@ -1,5 +1,7 @@
-~/tmux/tmux attach -t default || ~/tmux/tmux new -s default
-
 function vim
   nvim $argv
 end
+
+set -gx PATH $PATH ~/code/java-microservices/make
+
+~/tmux/tmux attach -t default || ~/tmux/tmux new -s default
